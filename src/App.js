1575991a0c96f98811ac50a6c2478d798b5c551b
IamCarlMichael <carl.holmberg91@gmail.com>
2019-09-23 10:23:30 +0800

    Updated changes to allow react app to start with npm run start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,79 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import rawData from "./pokemon/pokemon";
+
+const SinglePokemon = props => {
+  return (
+    <div className={"PokemonCard"}>
+      <img src={require(`./pokemon/${props.id}.png`)} alt="Picture" />
+      <h2>{props.name}</h2>
+      <h4 className={"type"}>
+        {props.type.map(element => (
+          <p className={"elementtype"}>{element}</p>
+        ))}
+      </h4>
+      <div className={"stats"}>
+        <p>{"HP: " + props.hp}</p>
+        <p>{"Attack: " + props.Attack}</p>
+        <p>{"SpAttack: " + props.SpAttack}</p>
+        <p>{"SpDefence: " + props.SpDefence}</p>
+        <p>{"Speed: " + props.Speed}</p>
+      </div>
+    </div>
+  );
+};
+
+const MapPokemon = props => {
+  return props.map(item => (
+    <SinglePokemon
+      id={item.id}
+      name={item.name.english}
+      type={item.type}
+      hp={item.base.HP}
+      Attack={item.base.Attack}
+      SpAttack={item.base.SpAttack}
+      SpDefence={item.base.SpDefence}
+      Speed={item.base.Speed}
+    />
+  ));
+};
+
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonData: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://us-central1-pokedex-23fb6.cloudfunctions.net/app/pokemonData"
+    )
+      .then(res => res.json())
+      .then(resInJson =>
+        this.setState(state => {
+          return { pokemonData: resInJson };
+        })
+      );
+  }
+
+  DisplayAllPokemon = () => {
+    return MapPokemon(this.state.pokemonData);
+  };
+
+  render() {
+    return <div>{this.DisplayAllPokemon()}</div>;
+  }
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Pokemon />
+    </div>
+  );
 }
 
 export default App;
